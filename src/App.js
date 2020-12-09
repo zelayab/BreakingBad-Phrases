63c2f604fef86f53a21c1899d7a88495a3baa2eb
zelayab <73174174+zelayab@users.noreply.github.com>
2020-12-09 13:58:22 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React,{useState,useEffect} from 'react'
+import styled from '@emotion/styled';
+import Frase from './components/Frase';
+
+const Contenedor = styled.div`
+  display:flex;
+  align-items:center;
+  padding-top:5rem;
+  flex-direction:column;
+`
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007D35 0%, #007D35 40%, #0F574E 100%);
+  background-size: 310px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size:2rem;
+  border: 2px solid black;
+  transition: background-size .8s ease;
+
+  :hover{
+    cursor: pointer;
+    background-size: 500px;
+  }
+`;
+
+
+function App() {
+  
+  //state de frases
+  const [frase,guardarFrase] = useState({});
+
+
+  const consultarAPI = async () => {
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const frase = await api.json();
+    guardarFrase(frase[0]);
+  }
+
+  //cargar una frase al principio
+  useEffect (() => {
+    consultarAPI()
+  },[]);
+
+  return (
+    <Contenedor>
+        <Frase 
+          frase={frase}
+        />
+
+        <Boton
+          onClick={consultarAPI}
+        >
+            Push the boton
+        </Boton>
+      </Contenedor>
+  );
+}
+
+export default App;
